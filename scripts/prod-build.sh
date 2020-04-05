56afa52dd7b9be4a7e0c0ab74dfe9c5c516a86f1
Jim Brighter <jbrighter92@gmail.com>
2020-04-04 22:52:10 -0400

    move file edit to dockerfile, stop container before running new one

--- scripts/prod-build.sh
+++ scripts/prod-build.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-sed -i -e 's/\( CALL_API = *\)[^; ]*/\1true/' html/js/modules/weatherService.js
-
 docker pull nginx:1.17-alpine
 
 docker build -f Dockerfile -t jimbrighter/pi-dashboard:arm .
+
+docker stop pi-dashboard
 
 docker run -d --name pi-dashboard --rm -p 9001:80 jimbrighter/pi-dashboard:arm
 
