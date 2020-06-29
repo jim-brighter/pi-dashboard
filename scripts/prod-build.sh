5320eb8ba08b84acbe3088c6cf47572477cb386d
Jim Brighter <jbrighter92@gmail.com>
2020-06-28 22:12:04 -0400

    don't rm

--- scripts/prod-build.sh
+++ scripts/prod-build.sh
@@ -9,8 +9,9 @@ docker build -f Dockerfile -t jimbrighter/pi-dashboard:arm .
 if [ -n "$(docker ps -a | grep pi-dashboard | awk '{print $1}')" ]; then
     echo "stopping container..."
     docker stop pi-dashboard
+    docker rm pi-dashboard
 fi
 
-docker run -d --name pi-dashboard --rm -p 9001:80 --restart unless-stopped jimbrighter/pi-dashboard:arm
+docker run -d --name pi-dashboard -p 9001:80 --restart unless-stopped jimbrighter/pi-dashboard:arm
 
 docker push jimbrighter/pi-dashboard:arm
